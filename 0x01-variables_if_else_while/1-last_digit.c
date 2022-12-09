7d95e41e665d75e84ab114f59011ab05d2e4049c
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-09 13:33:48 -0800

    Task 2 multiple if and last digit comparison done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -32,5 +32,10 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
 	}
+
+	if (p < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
+	}
 	return (0);
 }
