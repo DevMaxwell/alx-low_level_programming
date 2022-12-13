9da5974076b204acbf7dbb2ebf42b9671bad78dc
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-13 13:46:17 -0800

    Task a-z completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Return: 0
+**/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	return (0);
+}
