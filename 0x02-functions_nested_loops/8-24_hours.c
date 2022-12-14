3f22f34494e0c267d87ca9edeb724b7530e4eb7b
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-14 01:30:55 -0800

    Task 8 completed

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer-Entry Point for function
+ * Return: 0
+**/
+
+void jack_bauer(void)
+{
+	int ho, hour, min, minutes;
+
+	for (ho = 0; ho <= 2; ho++)
+	{
+		for (hour = 0; hour <= 3; hour++)
+		{
+			for (min = 0; min <= 5; min++)
+			{
+				for (minutes = 0; minutes <= 9; minutes++)
+				{
+					_putchar(ho + '0');
+					_putchar(hour + '0');
+					_putchar(':');
+					_putchar(min + '0');
+					_putchar(minutes + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
