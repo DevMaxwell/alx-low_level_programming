2f42a3da0edff7b6951e591546291e8ce099037b
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-14 01:12:26 -0800

    Task 7 completed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit-Entry point to print the last number
+ * @l:parameter to be accepted
+ * Return:last digit
+**/
+
+int print_last_digit(int l)
+{
+	int digit;
+
+	digit = l % 10;
+	if (digit < 0)
+	{
+		digit = digit * -1;
+	}
+	_putchar(digit);
+	return (digit);
+}
