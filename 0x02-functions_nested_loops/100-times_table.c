03c5473d68482322e26871e6161f18f1c50d890b
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-14 12:37:06 -0800

    Advanced task 1 completed

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table-Entrypoint to print n multiplication table
+ * @n:parameter to be received
+ * Return:0
+**/
+
+void print_times_table(int n)
+{
+	int num, dig, ans;
+
+	if (n >= 0 &&  n < 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar(48);
+			for (dig = 1; dig <= n; dig++)
+			{
+				ans = num * dig;
+				_putchar(44);
+				_putchar(32);
+				if (ans <= 99)
+				{
+					_putchar(32);
+					_putchar((ans / 10) + 48);
+					_putchar((ans % 10) + 48);
+				}
+				else if (ans > 99)
+				{
+					_putchar(((ans / 100) % 10) + 48);
+					_putchar(((ans / 10) % 10) + 48);
+					_putchar((ans % 10) + 48);
+				}
+				else
+				{
+					_putchar(32);
+					_putchar(ans + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
