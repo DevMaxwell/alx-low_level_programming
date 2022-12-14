e58df509a542645d258e5aed2a836525a54e1175
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-14 01:36:16 -0800

    Task 8 completed and reviewed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,6 +16,10 @@ void jack_bauer(void)
 			{
 				for (minutes = 0; minutes <= 9; minutes++)
 				{
+					if (ho == 2 && hour  > 3)
+					{
+						break;
+					}
 					_putchar(ho + '0');
 					_putchar(hour + '0');
 					_putchar(':');
