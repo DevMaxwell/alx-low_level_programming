059217ae9331d8ea10f8dc329b4bfe4e25dd6c8c
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-14 05:47:53 -0800

    Task 10 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add-Entry Point foradd function
+ * @a: parameter one
+ * @b: parameter two
+ * Return:add of the two parameters
+**/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
