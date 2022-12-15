4b53b24befe3997e5be76c391c65061c6a9782cd
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-15 06:02:33 -0800

    Task 0 completed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper-Entry point to function
- * @c:parameter to be received
- * Return 1 if uppercase and 0 otherwise
+ * _isupper - Entry point to function.
+ * @c:parameter to be received.
+ * Return:1 if uppercase and 0 otherwise.
  */
 
 int _isupper(int c)
