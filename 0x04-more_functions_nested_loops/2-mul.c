56022dcb3584352f2df080c2be47e63c1e2c2005
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-15 06:17:41 -0800

    Task 2 completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry Point to function
+ * @a:parameter one
+ * @b:parameter two
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	_putchar(c);
+	_putchar('\n');
+	return (0);
+}
