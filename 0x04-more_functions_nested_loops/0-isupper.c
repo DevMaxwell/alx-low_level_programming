9dda32910a165b5b53c5843d057d0afd94568b78
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-15 05:58:10 -0800

    Task 0 done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper-Entry point to function
+ * @c:parameter to be received
+ * Return 1 if uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
