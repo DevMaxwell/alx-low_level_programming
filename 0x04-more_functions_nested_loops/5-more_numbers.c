b452ab584e6496a708be1c9dd128664dc930abce
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-15 09:42:32 -0800

    Task 5 completed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry Point to function
+ *
+ * Return:0
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+			else
+			{
+				_putchar(b + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
