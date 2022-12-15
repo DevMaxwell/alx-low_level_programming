dde87f26f42f460bdf4092890e0673b7d5b3d7c9
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-15 10:37:44 -0800

    Task 6 completed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line -Entry Point to function
+ * @n: parameter to be received
+ * Return:0
+ */
+
+void print_line(int n)
+{
+	int p;
+
+	for (p = 0; p < n; p++)
+	{
+		_putchar('_');
+	}
+	putchar('\n');
+}
