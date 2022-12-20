846069f31e95da639afc972a30264a7bb49ffc2e
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-19 20:08:55 -0800

    Advanced Task 2 completed

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+		{
+			break;
+		}
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
