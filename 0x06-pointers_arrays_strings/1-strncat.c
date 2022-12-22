f662616548adc38abfe887f7eb11c536ddebd44d
Maxwell <anoliefomaxwell2@gmail.com>
2022-12-22 14:35:34 -0800

    Task 1 completed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,23 +10,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i;
-	int j;
+	unsigned int i = 0;
+	int j = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 
-	j = 0;
-
-	while (j < n && src[j] != '\0')
+	while (*(src + j) != '\0')
 	{
-		dest[i] = src[j];
-		i++;
+		if (j < n)
+		{
+			*(dest + i) = *(src + j);
+			i++;
+		}
 		j++;
 	}
-	dest = '\0';
 	return (dest);
+}
 }
